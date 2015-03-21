e86ebe6c3ab341a2116fb9fc28e5070c4058ab64
gras <github@gras.org>
2015-03-21 15:29:21 -0400

    Initial commit

---
+++ LegoBot-15/src/main.py
@@ -0,0 +1,29 @@
+'''
+Created on Mar 14, 2015
+
+@author: botball
+'''
+from kovan import msleep
+from kovan import motor
+from kovan import ao
+
+import os
+import sys
+
+# set print to unbuffered
+sys.stdout = os.fdopen(sys.stdout.fileno(),'w',0)
+
+def main() :
+    print "Starting...."
+    testMotors()
+    print "Finished"
+
+def testMotors() :
+    motor(1,100)
+    motor(3,100)
+    msleep(500)
+    ao()
+
+
+if __name__ == "__main__":
+    main()
